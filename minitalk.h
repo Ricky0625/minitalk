a2654809414d07ba1b8e80b37efe98e650c84be9
Ricky Wong <wricky-t@student.42kl.edu.my>
2022-08-02 13:56:12 +0800

    init header file, added styling for text

---
+++ minitalk.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wricky-t <wricky-t@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/02 13:45:24 by wricky-t          #+#    #+#             */
+/*   Updated: 2022/08/02 13:55:03 by wricky-t         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// libraries
+#include <unistd.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <stdio.h> // for testing purpose
+
+// text styling colors
+#define GR "\033[1;92m"
+#define RD "\033[1;31m"
+#define DF "\033[0m"
